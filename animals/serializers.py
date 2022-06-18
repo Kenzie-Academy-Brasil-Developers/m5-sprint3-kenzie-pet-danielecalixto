a96635e0c5af5878cfd835629123daacbd463b71
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-18 16:44:10 -0300

    feat: add update to AnimalSerializer

--- animals/serializers.py
+++ animals/serializers.py
@@ -26,3 +26,12 @@ class AnimalSerializer(serializers.Serializer):
             charac, created = Characteristic.objects.get_or_create(**charac)
             animal.characteristics.add(charac)
         return animal
+
+    def update(self, instance, validated_data):
+        non_editable_keys = ("sex", "group")
+        for key, value in validated_data.items():
+            if key in non_editable_keys:
+                raise KeyError
+            setattr(instance, key, value)
+        instance.save()
+        return instance
