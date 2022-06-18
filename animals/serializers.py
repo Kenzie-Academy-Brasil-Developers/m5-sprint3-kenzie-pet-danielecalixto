644adf051990abccc44fd033f2c2d3d82539890f
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-18 15:57:00 -0300

    fix: AnimalSerializer cant create repeated group or charac

--- animals/serializers.py
+++ animals/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
-from groups.serializers import GroupSerializer
+
 from groups.models import Group
-from characteristics.serializers import CharacteristicSerializer
-from characteristics.models import Characteristic
 from .models import Animal
-# from animals.serializers import AnimalSerializer
-# serializer = AnimalSerializer(data=animal_data)
+from characteristics.models import Characteristic
+
+from groups.serializers import GroupSerializer
+from characteristics.serializers import CharacteristicSerializer
+
 class AnimalSerializer(serializers.Serializer):
     name = serializers.CharField()
     age = serializers.FloatField()
@@ -15,14 +16,12 @@ class AnimalSerializer(serializers.Serializer):
     characteristics = CharacteristicSerializer(many=True)
 
     def create(self, validated_data):
-        print(validated_data)
         characteristics_data = validated_data.pop('characteristics')
         group_data = validated_data.pop('group')
-        group = Group.objects.create(**group_data)
+        group, created = Group.objects.get_or_create(**group_data)
         group_id = group.id
-        characteristics_list = list()
+        animal = Animal.objects.create(group_id=group_id, **validated_data)
         for charac in characteristics_data:
-            characteristic = Characteristic.objects.create(**charac)
-            characteristics_list.append(characteristic.id)
-        animal = Animal.objects.create(group_id=group_id, characteristics=characteristics_list, **validated_data)
+            charac, created = Characteristic.objects.get_or_create(**charac)
+            animal.characteristics.add(charac)
         return animal
